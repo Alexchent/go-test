17fa53397ece1fe55a2c53f853670820bb26c7f3
Alexchent <1023615292@qq.com>
2020-01-08 21:28:08 +0800

    switch表达式

--- if.go
+++ if.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
    /* 定义局部变量 */
-   var a int = 10
+   var a int = 20
  
    /* 使用 if 语句判断布尔表达式 */
    if a < 20 {
        /* 如果条件为 true 则执行以下语句 */
        fmt.Printf("a 小于 20\n" )
+   } else {
+   	   fmt.Printf("a 大于等于20\n")
    }
    fmt.Printf("a 的值为 : %d\n", a)
 }
