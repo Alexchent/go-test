0da67bd7d0b83b867e82c9930e56a5c8f7462733
Alexchent <1023615292@qq.com>
2020-01-10 00:45:14 +0800

    并发 协程与chann

---
+++ goroutine.go
@@ -0,0 +1,27 @@
+package main 
+
+import "fmt"
+
+
+//轻量级线程——协程 实现并发
+
+func main() {
+	s := []int {7,2,8,-9,4,0}
+	//分治思想
+    c := make(chan int)
+    //开启协程
+    go sum(s[:len(s)/2], c)
+    go sum(s[len(s)/2:], c)
+    x, y := <-c, <-c //从通道中取数据
+
+    fmt.Println(x, y, x+y)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _,v := range s {
+		sum += v
+	}
+	//sum发送到信道c中
+	c <- sum
+}
