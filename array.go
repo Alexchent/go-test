24b12cbf5eca0d7d9da7960970cffeb45327545f
Alexchent <1023615292@qq.com>
2020-01-09 19:22:38 +0800

    多维数组的声明，和使用

--- array.go
+++ array.go
@@ -13,4 +13,14 @@ func main(){
 		n[i] = i +100
 		fmt.Printf("%d 位的值为 %d\n", i, n[i])
 	}
+
+
+    //声明一个多维数组
+	var a = [5][2]int {{0,0},{1,2},{2,4},{3,6},{4,8}}
+    var j int
+	for  i = 0; i < 5; i++ {
+      for j = 0; j < 2; j++ {
+         fmt.Printf("a[%d][%d] = %d\n", i,j, a[i][j] )
+      }
+   }
 }
