2611e4aac3b337acf1467ca3d299e21a06108b70
Alexchent <1023615292@qq.com>
2022-05-13 17:45:15 +0800

    清理redis中的js和torrent文件

--- Cache/redis.go
+++ Cache/redis.go
@@ -84,6 +84,36 @@ func SMembers(key string) (val []string) {
 	return
 }
 
+func SRem(key string, members ...interface{}) {
+	err := Client.SRem(ctx, key, members).Err()
+	if err == redis.Nil {
+		fmt.Println("key does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func SPop(key string) (val string) {
+	val, err := Client.SPop(ctx, key).Result()
+	if err == redis.Nil {
+		fmt.Println("key does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func SPopN(key string, count int64) (val []string) {
+	val, err := Client.SPopN(ctx, key, count).Result()
+	if err == redis.Nil {
+		fmt.Println("key does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+	return
+}
+
 func SScan(key string, cursor uint64, match string, count int64) (val []string) {
 	val, _, err := Client.SScan(ctx, key, cursor, match, count).Result()
 	if err == redis.Nil {
---
+++ scan_clear.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexchen/test/Cache"
+	"strings"
+)
+
+// 清理后缀是js和torrent的文件
+func main() {
+	key := "laravel_database_files"
+	val := Cache.SMembers(key)
+
+	for _, v := range val {
+		if strings.HasSuffix(v, "js") || strings.HasSuffix(v, "torrent") {
+			fmt.Println(v)
+			Cache.SRem(key, v)
+		}
+	}
+}
