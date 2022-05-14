f05bb095f621ab0c7d37eea5ce5951b5868d0ae1
Alexchent <1023615292@qq.com>
2022-05-14 15:53:46 +0800

    搜索时忽略大小写

--- sscan.go
+++ sscan.go
@@ -9,21 +9,36 @@ import (
 func main() {
 	var path string
 	fmt.Printf("请输入要查询的文件:\n")
-
 	_, err := fmt.Scan(&path)
 	if err != nil {
 		return
 	}
-	res := Cache.SMembers("have_save_file")
-	for _, val := range res {
-		if strings.Contains(val, path) {
-			fmt.Println(val)
-		}
-	}
+	path = strings.ToLower(path)
+	SearchFromRedisSet("have_save_file", path)
 
-	res2 := Cache.SMembers("laravel_database_files")
-	for _, val := range res2 {
-		if strings.Contains(val, path) {
+	SearchFromRedisSet("laravel_database_files", path)
+
+	//res := Cache.SMembers("have_save_file")
+	//for _, val := range res {
+	//	// 处理分割符
+	//	val = strings.ToLower(val)
+	//	if strings.Contains(val, path) {
+	//		fmt.Println(val)
+	//	}
+	//}
+
+	//res2 := Cache.SMembers("laravel_database_files")
+	//for _, val := range res2 {
+	//	if strings.Contains(strings.ToLower(val), path) {
+	//		fmt.Println(val)
+	//	}
+	//}
+}
+
+func SearchFromRedisSet(key, path string) {
+	res := Cache.SMembers(key)
+	for _, val := range res {
+		if strings.Contains(strings.ToLower(val), path) {
 			fmt.Println(val)
 		}
 	}
