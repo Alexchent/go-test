21fdad1c7f352298f3a384f48780715a1aa3e00e
chentao <chentao@zenmen.com>
2022-06-20 15:55:13 +0800

    命令行参数

---
+++ flag.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var (
+	id   = flag.Int("id", 123, "Name to greet")
+	date = flag.String("date", time.Now().Format("20060102"), "时间")
+)
+
+//  go run flag.go --id=89 --date=20220624
+func main() {
+
+	flag.Parse() //解析命令行参数
+
+	fmt.Println("id:", *id)
+
+	fmt.Println("date:", *date)
+}
