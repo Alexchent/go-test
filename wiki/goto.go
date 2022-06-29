749d9a5b4e0d0d689de7313870c44d5d31d4532a
chentao <chentao@zenmen.com>
2022-06-29 17:02:27 +0800

    流程语句 if，for，goto

--- wiki/goto.go
+++ wiki/goto.go
@@ -1,17 +1,19 @@
 package main
 
 import "fmt"
+
 // goto不推荐使用。以免造成程序流程混乱
 func main() {
 	var a int = 10
 
 	//LOOP为标记位置
-	LOOP: for a < 20 {
+LOOP:
+	for a < 20 {
 		if a == 15 {
-			a +=1
+			a += 1
 			goto LOOP //goto程序直接跳转到goto位置
 		}
-		fmt.Printf("a的值：%d\n",a)
+		fmt.Printf("a的值：%d\n", a)
 		a++
 	}
 }
