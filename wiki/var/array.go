49d20ac022c993443ac488a14353857040fbfc36
chentao <chentao@zenmen.com>
2022-06-30 17:28:27 +0800

    slice与array的区别

--- wiki/var/array.go
+++ wiki/var/array.go
@@ -2,26 +2,41 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	//初始化7个int型成员的数组
+	var week = [7]int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(week[1])
+	//修改数组成员
+	week[1] = 10
+	fmt.Println(week[1])
+
+	//初始化7个string型成员的数组
+	weekname := [7]string{"周日", "周一", "周二", "周三", "周四", "周五", "周六"}
+	fmt.Println(weekname[1])
+
+	//初始化不确定长度的int型数组
+	userids := [...]int{1, 2, 3, 9, 2, 3, 6}
+	fmt.Println(userids, len(userids), cap(userids))
+
 	/* n是一个长度为10，元素类型为int的数组*/
 	var n [10]int
 
 	var i int
 
-	for i = 0; i<10; i++ {
+	for i = 0; i < 10; i++ {
 		//修改元素的值
-		n[i] = i +100
+		n[i] = i + 100
 		fmt.Printf("%d 位的值为 %d\n", i, n[i])
 	}
-    
-    fmt.Println(n)
 
-    //声明一个多维数组
-	var a = [5][2]int {{0,0},{1,2},{2,4},{3,6},{4,8}}
-    var j int
-	for  i = 0; i < 5; i++ {
-      for j = 0; j < 2; j++ {
-         fmt.Printf("a[%d][%d] = %d\n", i,j, a[i][j] )
-      }
-   }
+	fmt.Println(n)
+
+	//声明一个多维数组
+	var a = [5][2]int{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+	var j int
+	for i = 0; i < 5; i++ {
+		for j = 0; j < 2; j++ {
+			fmt.Printf("a[%d][%d] = %d\n", i, j, a[i][j])
+		}
+	}
 }
--- wiki/var/slice-array.go
+++ wiki/var/slice-array.go
@@ -3,37 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	a := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// 数组是比较特殊的，var 声明时已经带上长度，值永远不可能是nil, 因此可以通过 a[index]=value 的方式赋值
+	var a [2]int
+	var b []int
 
-	slice1 := a[2:4:8]
-	fmt.Println(slice1) // [2, 3]
-
-	slice2 := slice1[1:6] // 注意切片长度不能越过容量
-	fmt.Println(slice2)   // [3, 4, 5, 6, 7]
+	a[0] = 10 // 数组是比较特殊的
+	//b[0] = 10 // panic: runtime error: index out of range [0] with length 0
+	b = append(b, 10, 20)
+	fmt.Println(a, b)
 
 	testSlice()
-
 	testMap()
 }
 
+// 切片作为函数的参数，传的是指针
 func testSlice() {
 	x := []int{1, 2, 3}
 
 	func(arr []int) {
-		arr[0] = 7
+		arr[1] = 7
 		fmt.Println(arr)
 	}(x)
-	fmt.Println("slice类型的变量发生改变")
+	fmt.Println("slice类型的变量 —— 改变")
 	fmt.Println(x)
 }
 
+// 切片作为函数的参数，传的是值
 func testMap() {
 	x := [3]int{1, 2, 3}
 
 	func(arr [3]int) {
-		arr[0] = 7
+		arr[1] = 7
 		fmt.Println(arr)
 	}(x)
-	fmt.Println("array类型的变量没有改变")
+	fmt.Println("array类型的变量 —— 不变")
 	fmt.Println(x)
 }
