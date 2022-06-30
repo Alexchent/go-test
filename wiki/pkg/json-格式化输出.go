17540cf34593c63e37ed4b33ec86ef52f514a79a
chentao <chentao@zenmen.com>
2022-06-30 17:53:16 +0800

    json格式化输出

---
+++ wiki/pkg/json-格式化输出.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name  string
+	Hobby string
+}
+
+func main() {
+	p := Person{"5lmh.com", "女"}
+	// 编码json
+	b, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println("json err ", err)
+	}
+
+	// 格式化输出
+	b, err = json.MarshalIndent(p, "", "	")
+	if err != nil {
+		fmt.Println("json err ", err)
+	}
+	fmt.Println(string(b))
+}
