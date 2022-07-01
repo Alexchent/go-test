9dd725eca7bbd93b7d5d3c32faffd167475bd3c3
chentao <chentao@zenmen.com>
2022-07-01 11:16:50 +0800

    清理代码

--- sscan.go
+++ sscan.go
@@ -17,22 +17,6 @@ func main() {
 	SearchFromRedisSet("have_save_file", path)
 
 	SearchFromRedisSet("laravel_database_files", path)
-
-	//res := Cache.SMembers("have_save_file")
-	//for _, val := range res {
-	//	// 处理分割符
-	//	val = strings.ToLower(val)
-	//	if strings.Contains(val, path) {
-	//		fmt.Println(val)
-	//	}
-	//}
-
-	//res2 := Cache.SMembers("laravel_database_files")
-	//for _, val := range res2 {
-	//	if strings.Contains(strings.ToLower(val), path) {
-	//		fmt.Println(val)
-	//	}
-	//}
 }
 
 func SearchFromRedisSet(key, path string) {
