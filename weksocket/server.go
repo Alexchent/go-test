0143e359e65aa9eceaa15818496db50c77612718
chentao <chentao@zenmen.com>
2022-07-28 18:28:16 +0800

    websocket demo

--- weksocket/server.go
+++ weksocket/server.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
 
-func wsHandler(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("hello"))
+//设置websocket
+//CheckOrigin防止跨站点的请求伪造
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("hello"))
+
+	//升级get请求为webSocket协议
+	ws, err := upGrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close() //返回前关闭
+
+	for {
+		//读取ws中的数据
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		log.Print(string(message))
+
+		//写入ws数据
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
+	}
 }
 
 func main() {
