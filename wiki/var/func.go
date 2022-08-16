c418b37d3d8fc0b51b247bdbd029e420aef7157d
chentao <chentao@zenmen.com>
2022-08-16 11:40:00 +0800

    格式化项目 gofmt ../gotest

--- wiki/var/func.go
+++ wiki/var/func.go
@@ -2,24 +2,23 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	a := 100
 	b := 200
 	var ret int
-    ret = max(a,b)
-    fmt.Printf("最大值是：%d\n", ret)
+	ret = max(a, b)
+	fmt.Printf("最大值是：%d\n", ret)
 
-
-    x,y := swap("google", "webkit")
-    fmt.Println(x,y)
+	x, y := swap("google", "webkit")
+	fmt.Println(x, y)
 
 }
 
 //声明参数名称和类型 声明返回值类型
 func max(num1, num2 int) int {
 	var res int
-	if (num1 > num2) {
+	if num1 > num2 {
 		res = num1
 	} else {
 		res = num2
@@ -31,5 +30,3 @@ func max(num1, num2 int) int {
 func swap(x, y string) (string, string) {
 	return y, x
 }
-
-
