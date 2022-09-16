500080cbc45d1d3cde3d5fedeb410f446a610671
chentao <chentao@zenmen.com>
2022-09-16 18:08:00 +0800

    清理废代码

--- scan/scan_clear.go
+++ scan/scan_clear.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/alexchen/go_test/Cache/redis"
-	"log"
-	path "path/filepath"
 	"strings"
 )
 
@@ -15,18 +13,13 @@ func main() {
 	for _, v := range val {
 		//fmt.Println(v)
 		// 清理末尾有换行符的值
-		fileExt := path.Ext(v)
-		//fmt.Println(fileExt)
-		log.Print(fileExt)
-
 		newv := strings.TrimRight(v, "\n")
 		redis.SRem(key, v)
 		redis.SAdd(key, newv)
-
 		//fmt.Println(newv)
 		//return
-		// 按 后缀清理
 
+		// 按 后缀清理
 		if strings.HasSuffix(v, "js") || strings.HasSuffix(v, "torrent") {
 			fmt.Println(v)
 			redis.SRem(key, v)
