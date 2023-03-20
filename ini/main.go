8022f9d1293043c9290f36557f467c328466e5a7
chentao <chentao@zenmen.com>
2023-03-20 14:43:47 +0800

    ini.v1 处理ini文件

--- ini/main.go
+++ ini/main.go
@@ -19,29 +19,25 @@ func main() {
 	secs := cfg.SectionStrings()
 	fmt.Println(secs)
 
-	return
 	// 读取配置，默认分区使用空字符串表示
 	app_mode := cfg.Section("").Key("app_mode").String()
-	fmt.Println(app_mode)
+	fmt.Println("app_mode: " + app_mode)
 	data_path := cfg.Section("paths").Key("data").String()
-	fmt.Println(data_path)
+	fmt.Println("path.data:" + data_path)
 
 	// 候选值限制
-	fmt.Println("Server Protocol:", cfg.Section("server").Key("protocol").In("http", []string{"http", "https"}))
+	fmt.Println("Server.Protocol:", cfg.Section("server").Key("protocol").In("http", []string{"http", "https"}))
 	// 如果读取的值不再候选列表内，则返回默认值
-	fmt.Println("Email Protocol:", cfg.Section("server").Key("protocol").In("smtp", []string{"imap", "smtp"}))
+	fmt.Println("Email.Protocol:", cfg.Section("server").Key("protocol").In("smtp", []string{"imap", "smtp"}))
 
 	// 自动类型转换
-	fmt.Println("port number: ", cfg.Section("server").Key("http_port").MustInt(999))
-	fmt.Println("enforce domain: ", cfg.Section("server").Key("enforce_domain").MustBool(false))
+	fmt.Println("server.http_port: ", cfg.Section("server").Key("http_port").MustInt(999))
+	fmt.Println("server.enforce_domain: ", cfg.Section("server").Key("enforce_domain").MustBool(false))
 
 	// 修改配置文件
 	cfg.Section("").Key("app_mode").SetValue("production")
-	cfg.SaveTo("my-local.ini")
-
-}
-
-func testSection() {
-	sec, _ := cfg.GetSection("")
-	fmt.Println(sec)
+	err = cfg.SaveTo("my-local.ini")
+	if err != nil {
+		panic(err)
+	}
 }
