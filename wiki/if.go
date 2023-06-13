f247962dbc9daecafdfcf4daf91141ac2474f938
chentao <chentao@zenmen.com>
2023-06-13 15:36:35 +0800

    演示：switch case

--- wiki/if.go
+++ wiki/if.go
@@ -14,4 +14,35 @@ func main() {
 		fmt.Printf("a 大于等于20\n")
 	}
 	fmt.Printf("a 的值为 : %d\n", a)
+
+	season := Season(7)
+	fmt.Println(season)
+
+	Loop()
+}
+
+func Season(month int) string {
+	switch month {
+	case 1, 2, 12:
+		return "winter"
+	case 3, 4, 5:
+		return "spring"
+	case 6, 7, 8:
+		return "summer"
+	case 9, 10, 11:
+		return "Autumn"
+	}
+	return "unknown"
+}
+
+func Loop() {
+LABEL1:
+	for i := 0; i <= 5; i++ {
+		for j := 0; j <= 5; j++ {
+			if j == 4 {
+				continue LABEL1
+			}
+			fmt.Printf("i is %d, and j is : %d\n", i, j)
+		}
+	}
 }
