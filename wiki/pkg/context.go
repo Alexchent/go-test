1a214f836706c149697c23cbdba7ba582ca9fb03
Alexchent <1023615202@qq.com>
2023-10-08 23:23:49 +0800

    context包的使用场景

---
+++ wiki/pkg/context.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+// 传递取消信号，结束任务
+
+func main() {
+	mock()
+}
+
+func mockCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		// 模拟子 goroutine 持续工作
+		for range time.Tick(1 * time.Second) {
+			select {
+			case <-ctx.Done():
+				println("goroutine exit")
+				return
+			default:
+				println("goroutine running")
+			}
+		}
+	}()
+
+	time.Sleep(3 * time.Second)
+	// 通知子 goroutine 退出
+	cancel()
+	// 等待子 goroutine 退出
+	time.Sleep(1 * time.Second)
+}
+
+// 传递超时信号，结束任务
+func mock() {
+	// 3s 后超时
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// 模拟子 goroutine 持续工作
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		// 通知子 goroutine 退出
+		cancel()
+	}()
+
+	select {
+	case <-ctx.Done():
+		println("完成任务")
+		return
+	case <-time.After(10 * time.Second):
+		println("任务超时")
+	}
+}
+
+type key int
+
+const (
+	requestIDKey key = iota
+)
+
+// 传递上下文信息
+func WithRequestId(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// 从请求中提取请求ID和用户信息
+		requestID := req.Header.Get("X-Request-ID")
+
+		// 创建子 context，并添加一个请求 Id 的信息
+		ctx := context.WithValue(req.Context(), requestIDKey, requestID)
+
+		// 创建一个新的请求，设置新 ctx
+		req = req.WithContext(ctx)
+
+		// 将带有请求 ID 的上下文传递给下一个处理器
+		next.ServeHTTP(rw, req)
+	})
+}
