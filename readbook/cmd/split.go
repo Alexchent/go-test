33371dfea838643265e9acd156c97633a1de5b15
陈涛 <chentao02@SH-chentao02.local>
2024-06-26 20:37:30 +0800

    分割txt文件

--- readbook/cmd/split.go
+++ readbook/cmd/split.go
@@ -23,7 +23,7 @@ var splitCmd = &cobra.Command{
 	Short: "txt文本切割",
 	Long:  `txt文本切割`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := SplitFile(file, 5)
+		err := SplitFile(file, 5, 1, 20)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
@@ -36,7 +36,44 @@ func init() {
 	rootCmd.AddCommand(splitCmd)
 }
 
-func SplitFile(f string, line int) error {
+func SplitFile(f string, line int, startLine, endLine int) error {
+	open, err := os.Open(f)
+	defer open.Close()
+
+	utf8Reader := transform.NewReader(open, simplifiedchinese.GBK.NewDecoder()) // 编码 GBK转为UTF-8
+	fileScanner := bufio.NewScanner(utf8Reader)
+	lines := 1
+	var builder strings.Builder
+	builder.Grow(1000000000)
+	for fileScanner.Scan() {
+		// 将收到的GBK内容转换成utf-8
+		context := strings.TrimSpace(fileScanner.Text())
+		if len(context) == 0 {
+			continue
+		}
+		if lines < startLine {
+			//fmt.Println("忽略：" + context + "\n")
+			lines++
+			continue
+		}
+		//context = strings.Replace(context, "，", "", -1)
+		//context = strings.Replace(context, "。", "", -1)
+		builder.WriteString(context)
+		if lines%line == 0 {
+			fmt.Println("==================")
+			content := builder.String()
+			fmt.Println(content)
+			builder.Reset()
+		}
+		if lines >= endLine {
+			return nil
+		}
+		lines++
+	}
+	return err
+}
+
+func SplitFileAndTTs(f string, line int) error {
 	open, err := os.Open(f)
 	defer open.Close()
 
