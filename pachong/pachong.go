843fcd447392b64055792cde3b35a3fe315ceb9e
Alexchent <1023615202@qq.com>
2024-07-04 23:16:33 +0800

    并发执行爬虫

--- pachong/pachong.go
+++ pachong/pachong.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	file "github.com/alexchen/go_test/File"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // 下载书籍
@@ -18,21 +18,40 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	down1(homeDir)
+	run(homeDir+"/武侠之无尽恶人", "https://www.qixiashu.com/yue/62/62422/", 133837, 134262)
+	//StartWork(939847, 940506, "https://www.qixiashu.com/yue/465/465479/", homeDir+"/影视实际从攻略女主开始")
+	//StartWork(133837, 134262, "https://www.qixiashu.com/yue/62/62422/", homeDir+"/武侠之无尽恶人")
 }
 
-func down1(saveDir string) {
-	begin, end := 939847, 940506
-	host := "https://www.qixiashu.com/yue/465/465479/"
-	dir := saveDir + "/影视实际从攻略女主开始"
-	StartWork(begin, end, host, dir)
+// 并发下载
+func run(saveDir, host string, begin, end int) {
+	file.CreateDateDir(saveDir)
+	fmt.Println(saveDir)
+	var wg sync.WaitGroup
+	worker := 8
+	ch := make(chan int, worker)
+	// 启动4个消费者
+	wg.Add(worker)
+	for i := 0; i <= worker; i++ {
+		go download(host, saveDir, ch, &wg)
+	}
+
+	// 生产者
+	for begin <= end {
+		ch <- begin
+		begin++
+	}
+	close(ch)
+	wg.Wait()
 }
 
-func down2(saveDir string) {
-	begin, end := 133733, 138155
-	host := "https://www.qixiashu.com/yue/62/62422/"
-	dir := saveDir + "/武侠之无尽恶人"
-	StartWork(begin, end, host, dir)
+func download(host, saveDir string, ch <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for id := range ch {
+		url := host + strconv.Itoa(id) + ".html"
+		//fmt.Println(url)
+		getContent(url, saveDir)
+	}
 }
 
 // StartWork 开始工作 begin起始页 dir保存目录
@@ -40,9 +59,9 @@ func StartWork(begin, end int, host, saveDir string) {
 	var url string
 	file.CreateDateDir(saveDir)
 	fmt.Println(saveDir)
-	for begin < end {
+	for begin <= end {
 		url = host + strconv.Itoa(begin) + ".html"
-		fmt.Println(url)
+		//fmt.Println(url)
 		getContent(url, saveDir)
 		begin++
 		//time.Sleep(time.Second * 1) // 限制爬虫的速率
@@ -52,17 +71,20 @@ func StartWork(begin, end int, host, saveDir string) {
 func getContent(url, savePath string) {
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		fmt.Println("status code error: %d %s", res.StatusCode, res.Status)
+		return
 	}
 
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 
 	title := doc.Find("h1").Text()
@@ -75,4 +97,5 @@ func getContent(url, savePath string) {
 		javascript := "<script type=\"text/javascript\">\n\tvar odiv=document.getElementById('htmlContent')\n\tvar aDiv=odiv.getElementsByTagName('p')\n\n    //var aDiv = document.getElementsByTagName('p');\n    var arr = [];\n    for(var i=0;i<aDiv.length;i++)\n    {\n        arr.push(aDiv[i]);\n    }\n    arr.sort(function(a,b){return a.getAttribute('data-id') - b.getAttribute('data-id')});\n\t\n    for(var i=0;i<arr.length;i++)\n    {\n\n\nodiv.appendChild(arr[i]);\n\t\n    }\n\n</script>"
 		file.AppendContent(savePath+"/"+title+".html", javascript)
 	}
+	return
 }
