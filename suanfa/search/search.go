6a561a0b8e97e8a2c1450e3380ca766329638ba3
Alexchent <1023615202@qq.com>
2024-07-06 16:51:43 +0800

    搜索算法：二分查找

--- suanfa/search/search.go
+++ suanfa/search/search.go
@@ -22,3 +22,39 @@ func BinarySearch(arr []int, target int) int {
 	}
 	return -1
 }
+
+// 二分查找
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+	for left <= right {
+		//mid := left + (right-left)/2
+		mid := (left + right) / 2
+		switch {
+		case arr[mid] == target:
+			return mid
+			// 如果中间值大于目标值，则在左边查找
+		case arr[mid] > target:
+			right = mid - 1
+			// 如果中间值小于目标值，则在右边查找
+		case arr[mid] < target:
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+// 一个数组。一个目标数，找两个数之和是目标数的下标
+func twoSum(nums []int, target int) []int {
+	// 用map存储数组元素和下标
+	numMap := make(map[int]int)
+	// 遍历数组
+	for i, num := range nums {
+		// 如果map中存在目标值减去当前值的key，则返回
+		if j, ok := numMap[target-num]; ok {
+			return []int{j, i}
+		}
+		// 否则将当前值存入map，这样
+		numMap[num] = i
+	}
+	return nil
+}
