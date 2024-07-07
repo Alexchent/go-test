2dc3709d6e2a0402534c028009acd09ef24aab14
Alexchent <1023615202@qq.com>
2024-07-07 19:51:42 +0800

    优化

--- scan/sscan.go
+++ scan/sscan.go
@@ -8,23 +8,21 @@ import (
 )
 
 func main() {
-	var path string
+	var keyWord string
 	fmt.Printf("请输入要查询的文件:\n")
-	_, err := fmt.Scan(&path)
+	_, err := fmt.Scan(&keyWord)
 	if err != nil {
 		return
 	}
-	path = strings.ToLower(path)
-	count := 0
-	count += SearchFromRedisSet("have_save_file", path)
-	count += SearchFromRedisSet("laravel_database_files", path)
+
+	keyWord = strings.ToLower(keyWord)
+	count := SearchFromRedisSet("have_save_file", keyWord)
 	res := fmt.Sprintf("本次扫描发现 %d 个文件", count)
 	fmt.Println(res)
 }
 
 func SearchFromRedisSet(key, path string) (count int) {
 	res := redis.SMembers(key)
-	count = 0
 	//过滤掉特殊字符-和_
 	reg, _ := regexp.Compile("-|_")
 	path = reg.ReplaceAllString(path, "")
