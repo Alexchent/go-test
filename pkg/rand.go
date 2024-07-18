3f404a4b993030910b6f9c17dc0b036cea33d894
陈涛 <chentao02@SH-chentao02.local>
2024-07-18 11:31:18 +0800

    go生成随机数

--- pkg/rand.go
+++ pkg/rand.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().Unix()) //要产生随机数，必须初始化，否则每次产生的数都一样
-	fmt.Println(rand.Intn(5))
-	fmt.Println(rand.Intn(5))
+	// 使用当前时间的纳秒值作为种子
+	// 要产生随机数，必须初始化，否则每次产生的数都一样
+	//rand.Seed(time.Now().Unix())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	// 生成 0 到 100 之间的随机数
+	fmt.Println(rand.Intn(101))
 }
