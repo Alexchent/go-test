579a9202dae200cdb24de216f34ea9a3af258348
陈涛 <chentao02@SH-chentao02.local>
2024-08-07 20:06:06 +0800

    elasticsearch学习

--- elasticsearch/main.go
+++ elasticsearch/main.go
@@ -13,8 +13,10 @@ func main() {
 	conf.Elastic.Host = "127.0.0.1"
 	conf.Elastic.Port = 9200
 	m := model.NewUserES(model.NewEsClient(&conf))
+	t1 := uint64(time.Now().Second())
 	data := []*types.UserEs{
-		{ID: 100, UpdateTime: uint64(time.Now().Second()), CreateTime: uint64(time.Now().Second())},
+		{ID: 100, UpdateTime: t1, CreateTime: t1, Nickname: "章泽天"},
+		{ID: 101, UpdateTime: t1, CreateTime: t1, Nickname: "张学友"},
 	}
 	err := m.BatchAdd(context.Background(), data)
 	if err != nil {
--- elasticsearch/types/User.go
+++ elasticsearch/types/User.go
@@ -1,7 +1,13 @@
 package types
 
 type UserEs struct {
-	ID         uint64
-	UpdateTime uint64
-	CreateTime uint64
+	ID         uint64 `json:"id,omitempty" mapstructure:"id"`
+	Username   string `json:"username,omitempty" mapstructure:"username"`
+	Nickname   string `json:"nickname,omitempty" mapstructure:"nickname"`
+	Phone      string `json:"phone,omitempty" mapstructure:"phone"`
+	Age        uint64 `json:"age,omitempty" mapstructure:"age"`
+	Ancestral  string `json:"ancestral,omitempty" mapstructure:"Ancestral"`
+	Identity   string `json:"identity,omitempty" mapstructure:"identity"`
+	UpdateTime uint64 `json:"update_time,omitempty" mapstructure:"update_time"`
+	CreateTime uint64 `json:"create_time,omitempty" mapstructure:"create_time"`
 }
