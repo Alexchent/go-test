96096876ff0cc9b75b3ffd41a0804278b005a039
Alexchent <1023615202@qq.com>
2024-08-17 21:15:53 +0800

    三数之和

--- suanfa/leetcode/hash.go
+++ suanfa/leetcode/hash.go
@@ -88,3 +88,41 @@ func max(a, b int) int {
 		return b
 	}
 }
+
+// ThreeSum 三数之和
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+// 同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+func ThreeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	n := len(nums)
+	var result [][]int
+	for i := 0; i < n-2; i++ {
+		if nums[i] > 0 {
+			return result
+		}
+		// 去重
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, n-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				if left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				if left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return result
+}
--- suanfa/leetcode/hash_test.go
+++ suanfa/leetcode/hash_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"sort"
 	"testing"
 )
 
@@ -44,42 +43,4 @@ func TestCandy(t *testing.T) {
 			}
 		})
 	}
-}
-
-func ThreeSum(nums []int) [][]int {
-	var res [][]int
-	if len(nums) < 3 {
-		return res
-	}
-
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-2; i++ {
-		if nums[i] > 0 {
-			return res
-		}
-		// 去重
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
-		}
-		left, right := i+1, len(nums)-1
-		for left < right {
-			sum := nums[i] + nums[left] + nums[right]
-			if sum == 0 {
-				res = append(res, []int{nums[i], nums[left], nums[right]})
-				if left < right && nums[left] == nums[left+1] {
-					left++
-				}
-				if left < right && nums[right] == nums[right-1] {
-					right--
-				}
-				left++
-				right--
-			} else if sum < 0 {
-				left++
-			} else {
-				right--
-			}
-		}
-	}
-	return res
 }
