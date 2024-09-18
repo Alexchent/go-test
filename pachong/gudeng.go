6acd71b47e3347e8dcae6767ed2c6b48efc2a534
陈涛 <chentao02@SH-chentao02.local>
2024-09-18 17:58:57 +0800

    爬虫脚本: 分析html文件

--- pachong/gudeng.go
+++ pachong/gudeng.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/PuerkitoBio/goquery"
+	"strings"
 )
 
 // golang goquery selector(选择器) 示例大全 https://cloud.tencent.com/developer/article/1196783
 
-func ExampleScrape(url string) {
+func ScrapeFromUrl(url string) {
 	// Request the HTML page.
 	res, err := http.Get(url)
 	if err != nil {
@@ -22,9 +23,23 @@ func ExampleScrape(url string) {
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
+	work(res.Body)
+}
 
+func ScrapeFromFile(url string) {
+	file, err := os.Open(url)
+	if err != nil {
+		fmt.Println("http.get err:" + err.Error())
+		return
+	}
+	defer file.Close()
+
+	work(file)
+}
+
+func work(r io.Reader) {
 	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
 		log.Fatalf("goquery.NewDocumentFromReader err: %s", err.Error())
 	}
@@ -36,21 +51,22 @@ func ExampleScrape(url string) {
 	// Find the review items
 	doc.Find("body").Each(func(i int, s *goquery.Selection) {
 		// 获取p标签，且id属性包含id的元素
-		//node := s.Find("p[id*=id]")
-		//a, ok := node.Attr("id")
-		//if ok {
-		//	fmt.Println(a)
-		//}
-		content := s.Find("p[id*=id]").Text()
-		_, err2 := fd.WriteString(content + "\r\n")
-		if err2 != nil {
-			return
-		}
+		// content := s.Find("p[id*=id]").Text()
+		s.Find("p[id*=id]").Each(func(ii int, sc *goquery.Selection) {
+			content := sc.Text()
+			fmt.Println(content)
+			fmt.Println(strings.Repeat("=", 20))
+			_, err2 := fd.WriteString(content + "\r\n")
+			if err2 != nil {
+				return
+			}
+		})
 	})
 }
 
 func main() {
 	//url := "https://www.gutenberg.org/cache/epub/25271/pg25271-images.html"
-	url := "https://www.gutenberg.org/cache/epub/23962/pg23962-images.html"
-	ExampleScrape(url)
+	//url := "https://www.gutenberg.org/cache/epub/23962/pg23962-images.html"
+	//ScrapeFromUrl(url)
+	ScrapeFromFile("xixiangji.html")
 }
